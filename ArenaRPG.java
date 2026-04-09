bd8bc916e8ba5df9c4837d4d3b3e0d1ffd18be23
karizx <40469@student.atar.edu.pl>
2026-04-09 14:35:00 +0200

    Replace IO with System.out for console output

--- ArenaRPG.java
+++ ArenaRPG.java
@@ -1,31 +1,32 @@
+import java.util.Scanner;
 void main() {
     Scanner scanner = new Scanner(System.in);
     Hero gracz = new Hero("Geralt", 100, 25);
     Hero potwor = new Mage("Gandalf", 80, 15);
-    IO.println("Witaj na arenie! Przeciwnikiem jest: " + potwor.getName());
-    IO.println("--- ROZPOCZYNA SIĘ WALKA ---");
+    System.out.println("Witaj na arenie! Przeciwnikiem jest: " + potwor.getName());
+    System.out.println("--- ROZPOCZYNA SIĘ WALKA ---");
     while (gracz.getHealth() > 0 && potwor.getHealth() > 0) {
-        IO.println("\nTwoja tura! Twoje HP: " + gracz.getHealth() + " | HP Przeciwnika: " + potwor.getHealth());
-        IO.println("Wybierz akcję:");
-        IO.println("1 - Atak mieczem");
-        IO.println("2 - Wypij eliksir leczący");
-        IO.print("Twój wybór: ");
+        System.out.println("\nTwoja tura! Twoje HP: " + gracz.getHealth() + " | HP Przeciwnika: " + potwor.getHealth());
+        System.out.println("Wybierz akcję:");
+        System.out.println("1 - Atak mieczem");
+        System.out.println("2 - Wypij eliksir leczący");
+        System.out.print("Twój wybór: ");
         int wybor = scanner.nextInt();
         if (wybor == 1) {
             gracz.attack(potwor);
         } else if (wybor == 2) {
             gracz.heal();
         } else {
-            IO.println("Zły wybór, tracisz turę ze stresu!");
+            System.out.println("Zły wybór, tracisz turę ze stresu!");
         }
         if (potwor.getHealth() <= 0) {
-            IO.println("🏆 " + potwor.getName() + " pada na ziemię! Wygrywasz!");
+            System.out.println("🏆 " + potwor.getName() + " pada na ziemię! Wygrywasz!");
             break;
         }
-        IO.println("\nTura przeciwnika...");
+        System.out.println("\nTura przeciwnika...");
         potwor.attack(gracz);
         if (gracz.getHealth() <= 0) {
-            IO.println("☠️ Niestety, " + gracz.getName() + " ginie. Koniec gry.");
+            System.out.println("☠️ Niestety, " + gracz.getName() + " ginie. Koniec gry.");
             break;
         }
     }
